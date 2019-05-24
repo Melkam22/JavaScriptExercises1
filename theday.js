b2a85ba761d0f87fa876b46b5f0638e305c64815
Melkam22 <44436844+Melkam22@users.noreply.github.com>
2019-05-24 13:00:45 +0200

    Add files via upload

---
+++ theday.js
@@ -0,0 +1,24 @@
+
+function fullDate() {
+     
+    let myDate = new Date();
+    /* console.log */alert(myDate);
+
+    let getDate = myDate.getDate();//day in number
+  /*   console.log(getDate); */
+
+    let days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let getDaysName = days[myDate.getDay()];//day's name in letter
+    /* console.log(getDaysName); */
+     
+    let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let getMonthName = months[myDate.getMonth()];//month in letter
+    /* console.log(getMonthName); */
+
+    let getYear = myDate.getFullYear();//year in number
+    /* console.log(getYear); */
+
+    /* console.log */alert(`Today is : ${getDaysName} ${getDate}th of ${getMonthName} ${getYear} `);
+};
+fullDate();//we ll get sth like today is : 12 May 2019
+ 
